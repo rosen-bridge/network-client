0dd8e8d188e99cc7b56d906fbccbb85671cc0a0f
Mohammad Kermani <mkermani144@gmail.com>
2023-04-06 12:22:08 +0000

    feat: add `@rosen-clients/cardano-koios` package

---
+++ clients/cardano-koios/src/index.ts
@@ -0,0 +1,29 @@
+import { axiosInstance } from './axios';
+
+import * as address from './api/address';
+import * as asset from './api/asset';
+import * as block from './api/block';
+import * as epoch from './api/epoch';
+import * as network from './api/network';
+import * as pool from './api/pool';
+import * as script from './api/script';
+import * as stackAccount from './api/stake-account';
+import * as transactions from './api/transactions';
+
+const cardanoKoiosClientFactory = (baseURL: string) => {
+  axiosInstance.defaults.baseURL = baseURL;
+
+  return {
+    address,
+    asset,
+    block,
+    epoch,
+    network,
+    pool,
+    script,
+    stackAccount,
+    transactions,
+  };
+};
+
+export default cardanoKoiosClientFactory;
