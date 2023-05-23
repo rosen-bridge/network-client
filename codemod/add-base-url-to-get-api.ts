a1e3cf383ca74a134fd959fb40ef174a6107777a
vorujack <vorujack@gmail.com>
2023-05-23 10:17:45 +0330

    fix some comments

--- codemod/add-base-url-to-get-api.ts
+++ codemod/add-base-url-to-get-api.ts
@@ -2,7 +2,6 @@ import { API } from 'jscodeshift';
 
 export default (root: any, api: API) => {
   const j = api.jscodeshift;
-  // console.log(root.find(api.jscodeshift.ExportNamedDeclaration, {exportKind: 'value'}).length)
   root
     .find(api.jscodeshift.ExportNamedDeclaration, { exportKind: 'value' })
     .forEach((item) => {
@@ -17,7 +16,6 @@ export default (root: any, api: API) => {
           ])
         ),
       ]);
-      // const instanceDeclaration = j.identifier('const instance = axios.createInstance(url);')
       item.value.declaration.declarations[0].init.body.body.unshift(axios);
     });
 };
