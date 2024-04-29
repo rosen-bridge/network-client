b7e7948674d9ed7e40c4f69757625cf74ccdebc5
vorujack <vorujack@gmail.com>
2024-04-29 11:17:07 +0330

    fix versions

--- packages/axios/lib/index.ts
+++ packages/axios/lib/index.ts
@@ -47,8 +47,6 @@ export const JsonFieldBigintFactory = (fields: Array<string>) => {
   };
 };
 
-export const axiosInstance = Axios.create();
-
 export const createAxiosInstanceWithHeaders = (
   url: string,
   headers: { [key: string]: string }
