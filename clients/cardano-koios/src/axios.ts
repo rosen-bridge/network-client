3fcd4061b02c14427de26ff815332bc32a0ff869
vorujack <vorujack@gmail.com>
2024-04-29 11:05:44 +0330

    create axios package ignore constructor and proto field for json-bigint

--- clients/cardano-koios/src/axios.ts
+++ clients/cardano-koios/src/axios.ts
@@ -1,59 +1,9 @@
-import Axios, { AxiosRequestConfig } from 'axios';
-import JsonBigintFactory from 'json-bigint';
+import { createAxiosInstanceWithHeaders } from '@rosen-clients/axios';
+export {
+  JsonFieldBigintFactory,
+  createAxiosInstanceWithHeaders,
+} from '@rosen-clients/axios';
 
-const JsonBigInt = JsonBigintFactory({
-  alwaysParseAsBig: false,
-  useNativeBigInt: true,
-});
-
-const transformBigInt = (obj: any, bigIntObj: any, field: string): any => {
-  if (Array.isArray(obj)) {
-    return (obj as unknown as Array<JSON>).map((row, index) => {
-      return transformBigInt(
-        row,
-        (bigIntObj as unknown as Array<JSON>)[index],
-        field
-      );
-    }) as unknown as JSON;
-  }
-  if (!Object.hasOwn(obj, field.split('.')[0])) {
-    return {
-      ...obj,
-    };
-  }
-  if (field.indexOf('.') !== -1) {
-    const subKeys = field.split('.');
-    return {
-      ...obj,
-      [subKeys[0]]: transformBigInt(
-        obj[subKeys[0]],
-        bigIntObj[subKeys[0]],
-        subKeys.slice(1).join('.')
-      ),
-    };
-  }
-  return { ...obj, [field]: BigInt(bigIntObj[field]) };
-};
-export const JsonFieldBigintFactory = (fields: Array<string>) => {
-  return (data: any) => {
-    let dataJson = JSON.parse(data);
-    const dataBigInt = JsonBigInt.parse(data);
-    fields.forEach((field) => {
-      dataJson = transformBigInt(dataJson, dataBigInt, field);
-    });
-    return dataJson;
-  };
-};
-
-export const axiosInstance = Axios.create();
-
-export const createAxiosInstance = (url: string, authToken?: string) => {
-  const instance = Axios.create();
-  instance.defaults.baseURL = url;
-  if (authToken) {
-    instance.defaults.headers.common['Authorization'] = `Bearer ${authToken}`;
-  }
-  return <T>(config: AxiosRequestConfig): Promise<T> => {
-    return instance.request<T>(config).then((response) => response.data);
-  };
+export const createAxiosInstance = (url: string) => {
+  return createAxiosInstanceWithHeaders(url, {});
 };
