a1385d90087ad846c31b8a9813675645387f58b0
fateme.r <fatemeh.r3366@gmail.com>
2025-09-13 12:50:08 +0330

    move esline, typescript and @types/node to root package.json

--- packages/rate-limited-axios/test.ts
+++
@@ -1,6 +0,0 @@
-import { RateLimitedAxios } from './lib/RateLimitedAxios';
-
-const axios = new RateLimitedAxios();
-
-const response = await axios.get('https://api.github.com');
-console.log(response);
