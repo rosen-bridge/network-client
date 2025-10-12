3f9a2f4f00bfa38d1f6e8843f722e17ea98c7959
Mohsen Javidpanah <moshen212@gmail.com>
2025-10-12 13:24:49 +0330

    Export RateLimitedAxiosInstance instead of AxiosInstance

--- packages/rate-limited-axios/lib/index.ts
+++ packages/rate-limited-axios/lib/index.ts
@@ -39,7 +39,6 @@ import axios, {
   CancelTokenSource,
   AxiosInterceptorOptions,
   AxiosInterceptorManager,
-  AxiosInstance,
   GenericFormData,
   GenericHTMLFormElement,
   AxiosStatic,
@@ -84,6 +83,7 @@ const {
 export {
   rateLimitedAxios as default,
   RateLimitedAxios as Axios,
+  RateLimitedAxiosInstance as AxiosInstance,
   RateLimitedAxiosConfig,
   AxiosHeaderValue,
   AxiosHeaders,
@@ -131,7 +131,6 @@ export {
   CancelTokenSource,
   AxiosInterceptorOptions,
   AxiosInterceptorManager,
-  AxiosInstance,
   GenericFormData,
   GenericHTMLFormElement,
   getAdapter,
