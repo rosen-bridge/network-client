f8d8a029f9f4698c9a33fab220f1550bfddd79c0
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2025-10-13 16:29:01 +0330

    update orval and lint imports on ergo-explorer client

--- clients/ergo-explorer/src/axios.ts
+++ clients/ergo-explorer/src/axios.ts
@@ -1,4 +1,5 @@
 import { createAxiosInstanceWithHeaders } from '@rosen-clients/axios';
+
 export { JsonFieldBigintFactory } from '@rosen-clients/axios';
 
 export const createAxiosInstance = (url: string) => {
